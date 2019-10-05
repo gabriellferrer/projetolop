e2ce6a74a3cd51c36bd8b4892351c6e64c45a477
gabriellferrer <56178879+gabriellferrer@users.noreply.github.com>
2019-10-04 21:02:54 -0300

    Add files via upload

---
+++ etapa1-2.js
@@ -0,0 +1,26 @@
+<script>
+/* 
+Equipe: 
+Gabriel Lucas Aguiar Ferrer -  (Líder) 
+Etapa 1-2
+*/
+
+var x=200;
+var y=350;
+var xo=0
+var yo=50
+
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+ background(000);
+  ellipse(x, y, 25, 35);
+  rect(xo, yo, 35, 35);
+ }
+
+
+
+
+
+</script>
